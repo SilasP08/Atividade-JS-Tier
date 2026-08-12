eb487dfd30e4fe324196482076c288adefe6f581
SilasP0 <possarle.silas16@gmail.com>
2026-08-12 16:57:29 -0300

    Refactor Sobre component layout and styling

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -3,8 +3,13 @@ import "./sobre.css";
 export default function Sobre() {
     return (
         <>
-            <h1>pagina Sobre</h1>
-            <p>IT'S ALIVE!!</p>
+            <div className="conteiner">
+                <div className="conteudo">
+                    <h1>Sobre Nós</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quis eum exercitationem expedita ex facere minima error. Praesentium vel repellendus nobis laudantium ut quidem laboriosam explicabo delectus eveniet? Corporis, explicabo!</p>
+                    <img src="/image/logo.png" alt="" />
+                </div>
+            </div>
         </>
     );
 };
